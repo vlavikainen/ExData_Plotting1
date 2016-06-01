58f9f3340a47d2594fab9a14a655cfd13420d5fb
Ville Lavikainen <vlavikainen@gmail.com>
2016-06-01 20:42:37 +0300

    Fixin fourth code (Removing box in one legend)

--- plot4.R
+++ plot4.R
@@ -37,7 +37,7 @@ lines(data$Date, data$Sub_metering_2, col = "red")
 lines(data$Date, data$Sub_metering_3, col = "blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2",
                      "Sub_metering_3"), lty = c(1,1,1),
-       col=c("black", "red", "blue"))
+       col=c("black", "red", "blue"), bty = "n")
 
 ## ...and fourth
 plot(data$Date, data$Global_reactive_power, type = "l",
